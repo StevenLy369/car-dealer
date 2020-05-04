6fd270b363db5d161ddcb7d505de80dccbd814e6
Steven Ly <1stevenly92@gmail.com>
2020-05-04 16:22:52 -0700

    made code dryer

--- Car.cs
+++ Car.cs
@@ -9,27 +9,23 @@ public class Car{
     public bool Worthbuying(int maxPrice){
         return (Price < maxPrice);
     }
+
+    public Car(string MakeModel, int price, int miles){
+        MakeModel = MakeModel;
+        Price = price;
+        Miles = miles;
+    }
     
 }
 
 public class Program{
 
     public static void Main(){
-        Car audi = new Car();
-        audi.MakeModel = "2015 Audi s4";
-        audi.Price = 35000;
-        audi.Miles = 37000;
-
-        Car volkswagen = new Car();
-        volkswagen.MakeModel = "1974 Volkswagen Thing";
-        volkswagen.Price = 1100;
-        volkswagen.Miles = 368792;
-
-        Car ford = new Car();
-        ford.MakeModel = "1988 Ford Country Squire";
-        ford.Price = 1400;
-        ford.Miles = 239001;
-        List<Car> Cars = new List<Car>() { audi, volkswagen, ford };
+         Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
+        Car yugo = new Car("1980 Yugo Koral", 700, 56000);
+        Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
+      
+        List<Car> Cars = new List<Car>() { volkswagen, yugo, ford };
         List<Car> CarsMatchingSearch = new List<Car>(0); 
         Console.WriteLine("Enter Maxium price:");
         string stringMaxPrice = Console.ReadLine();
